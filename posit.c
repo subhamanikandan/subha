a174e7563f94edd8334ce2c8345ccfdee6c60367
subhamanikandan <35623152+subhamanikandan@users.noreply.github.com>
2018-01-21 09:39:06 +0530

    Create posit.c

---
+++ posit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enterthenumber\n");
+scanf("%d",&num);
+if(num>0)
+{
+printf("the number is positive");
+}
+else if("the number is negative");
+}
+else
+{
+printf("the number is zero");
+}
+}
