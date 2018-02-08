8d6c56b8dc25d9761ff45676781ce74f51f2dca2
subhamanikandan <35623152+subhamanikandan@users.noreply.github.com>
2018-02-08 09:30:36 +0000

    Create in.c

---
+++ in.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main
+{
+	int a[50],i,n;
+	printf("enter the size of array");
+	scanf("%d",a[i]);
+	printf("enter the elements\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("the index of %d is %d",i,a[i]);
+	}
+return 0;
+}
+
