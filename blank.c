d1e6af8df6f7e6221023ceeaf316111ff527f88a
subhamanikandan <35623152+subhamanikandan@users.noreply.github.com>
2018-02-13 09:23:21 +0000

    Create blank.c

---
+++ blank.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[50];
+int i,count=0;
+printf("\nEnter any string : ");
+gets(str);
+for(i=0;str[i];i++)
+if(str[i] == ' ')
+count++;
+printf("\nTotal blank spaces : %d",count);
+return 0;
+    }
