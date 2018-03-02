c3c23bfe32dfde62aa6c9c25fa40e9ecc5c939e1
subhamanikandan <35623152+subhamanikandan@users.noreply.github.com>
2018-03-02 16:26:08 +0530

    Create strconcat.c

---
+++ strconcat.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  char s1[25],s2[25];
+  int i=0,j=0;
+  printf("\nEnter First String:");
+  gets(s1);
+  printf("\nEnter Second String:");
+  gets(s2);
+  while(s1[i]!='\0')
+  i++;
+  while(s2[j]!='\0')
+  {
+    s1[i]=s2[j];
+    j++;
+    i++;
+  }
+  s1[i]='\0';
+  printf("\nConcatenated String is %s",s1);
+  return 0;
+}
