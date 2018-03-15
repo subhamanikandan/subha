12b246bf01687256eb4358883803c41ecb5cc23f
subhamanikandan <35623152+subhamanikandan@users.noreply.github.com>
2018-03-15 16:02:17 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+	int i,n,array[50],sum=0,avg=0;
+	printf("enter the size of the array");
+	scanf("%d",&n);
+	printf("enter the array elements");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+array[i];
+	}
+	avg=sum/n;
+	printf("\n%d",avg);
+	return 0;
+}
